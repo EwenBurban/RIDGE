754f710314dcabeba997424ab83837dcc1679dd3
Ewen Burban <eburban@cornuta.moulon.inrae.fr>
2023-04-06 07:28:29 +0200

    removed median and std ss from acp plots

--- core/QC_prior_sim.R
+++ core/QC_prior_sim.R
@@ -11,26 +11,23 @@ output_dir=args['output_dir']
 ddir=file.path(dir,'modelComp')
 prior_data=do.call(rbind,lapply(list.files(ddir,recursive=T,full.names=T,pattern='ABCstat_global.txt'),read.table,h=T))
 prior_data$tag='prior'
-print(head(prior_data))
 obs_data=read.table(file.path(dir,'ABCstat_global.txt'),h=T)
 obs_data$tag='obs'
 
 
 all_data=rbind(prior_data,obs_data)
 all_data_pca=subset(all_data,select=-c(dataset,tag))
+all_data_pca=all_data_pca[,colnames(all_data_pca)[-grep(colnames(all_data_pca),pattern='std|median')]]
 pca=PCA(all_data_pca,graph=F)
 ind_p=fviz_pca_ind(pca,habillage=as.factor(all_data$tag),label='n',addEllipse=T)
-var_p=fviz_pca_var(pca)
+var_p=fviz_pca_var(pca,repel=T)
 ggsave(ggarrange(ind_p,var_p),filename=file.path(output_dir,'QC_prior_acp.pdf'),width=14)
 
 
 param_2view=paste0(c('piA','piB','thetaA','thetaB','FST','divAB','netDivAB','sf','ss'),'_avg')
-print(param_2view)
 param_2view=c(param_2view,'sf_outlier','fst_outlier','divAB_outlier','netDivAB_outlier','piA_outlier','piB_outlier')
-print(param_2view)
 
 plot_list=lapply(param_2view,function(param,...){
-					 print(param)
 					 p=ggdensity(all_data,param,color='tag',fill='tag',title=param)+geom_vline(xintercept=obs_data[1,param],color='red')
 					 return(p)
 			})
