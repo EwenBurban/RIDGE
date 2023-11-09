e2dbaebab6ac4ad4b14fda61a96919309842f3f6
ewen <burban_ewen@mailfence.com>
2023-11-09 15:40:42 +0100

    ongoing work on BF
    improve estimate of pbarrier ratio when there is Q=0 and add aditional way to compute BF

--- core/barrier_detection.R
+++ core/barrier_detection.R
@@ -17,12 +17,24 @@ roc_smooth=0.001 # the lower the value is, the smoother the roc will be
 n_subset=5
 posterior_file=args['posterior']
 posterior=read.table(posterior_file,h=T)
-pbarrier=sapply(1:nrow(posterior),function(x,...) weighted.mean(posterior[x,c('PbarrierM_ancestral','PbarrierM_current')],w=c(posterior[x,'Tsplit']-posterior[x,'Tam'],posterior[x,'Tsc'])))
-v=(1-pbarrier)/(pbarrier)
+pbarrier=sapply(1:nrow(posterior),function(x,...){ 
 
-mean_prior_ratio=mean(v[is.infinite(v)==F],na.rm=T)
+					# this line weight the barrier proportion estimated for ancestral and current migration by their respective time of effect during divergence
+					# TO-DO : directly implement the pbarrier parameter in ABC model, allowing direct estimation of this parameter rather than estimating it at posteriori - E.BURBAN 9/11/2023
+				weighted.mean(posterior[x,c('PbarrierM_ancestral','PbarrierM_current')],w=c(posterior[x,'Tsplit']-posterior[x,'Tam'],posterior[x,'Tsc']))})
 
-######## defining functions ############
+######## pbarrier ratio computation (pbarrier is called Q in text)
+# correct way is to compute the average ratio (1-Q)/Q but this way produce Inf when Q=0. 
+# In model averaging, by construction some  posteriors might have a Q=0. 
+# Consequently, we remove Inf value from the mean and so increase the mean value. 
+# To avoid this pitfall, an aproximate way is to compute the ratio as mean(1-Q)/mean(Q)
+correct_pbarrier_ratio=(1-pbarrier)/(pbarrier)
+correct_mean_pbarrier_ratio=mean(v[is.infinite(v)==F],na.rm=T)
+aproxmate_mean_pbarrier_ratio=(1-mean(pbarrier))/mean(pbarrier)
+
+#mean_prior_ratio=
+
+######## defining local functions ############
 convert2bar = function(mvec){
 	bar_vec = ifelse(mvec == 0, 'barrier','non-barrier')
 	return(as.factor(bar_vec))
@@ -54,6 +66,7 @@ get_roc = function(prior,est,trsh,...){
 
 auc <-function(x, y, from = min(x, na.rm=TRUE), to = max(x, na.rm=TRUE),absolutearea=F,...)
 {
+	# AUC function from MASS R package
     # Sanity checks
     stopifnot(length(x) == length(y))
     stopifnot(!is.na(from))
@@ -78,16 +91,16 @@ auc <-function(x, y, from = min(x, na.rm=TRUE), to = max(x, na.rm=TRUE),absolute
 
 }
 
-#### load observed dataset ###############$
+#### load observed dataset ###############
 obs_data = read.table(file.path(obs_dir,'ABCstat_locus.txt'),h=T)
 if(mode=='test'){
+	# when applied to a pseudo-observed dataset where the true status of loci is know, this part define the column where to find migration value
+	# and provide a migration column when there is not. 
 	obs_prior=read.table(file.path(obs_dir,'priorfile_locus.txt'),h=T)
 	col=colnames(obs_prior)
 	if(any(colnames(obs_prior)=='M_current')){migration='M_current';print('M_current')
 	} else if (any(colnames(obs_prior)=='M_ancestral')){migration='M_ancestral';print('M_ancestral')}else{
-		migration='null_mig'; obs_prior[,'null_mig']=rep(1000,nrow(obs_prior));print('no_mig')}
-	if(length(unique(obs_prior[,migration]))==1){
-		migration='null_mig'; obs_prior[,'null_mig']=rep(1000,nrow(obs_prior));print('no_mig')}
+		migration='null_mig'; obs_prior[,'null_mig']=rep(0,nrow(obs_prior));print('no_mig')}
 	obs_all=merge(obs_data,obs_prior,by='dataset')
 }else {
 		migration='M_current'
@@ -108,32 +121,35 @@ obs_roc_all=all
 ### obs roc & prediction
 train_set=obs_roc_all[,col_data]
 prior_train_set=obs_roc_all[,col_prior]
-if(migration=='null_mig'){prior_train_set[,migration]=rep(1000,nrow(prior_train_set))}
+#if(migration=='null_mig'){prior_train_set[,migration]=rep(1000,nrow(prior_train_set))}
 status=	convert2bar(prior_train_set[,migration])
 train_set=data.frame(train_set,status)
 rf = abcrf(status~.,data=train_set,ncores = ncores,ntree = ntree,paral = T,lda=F)
 pred = predict(rf,training = train_set,obs = obs_data[,col_data],paral = T,ncores=ncores)
 if(mode=='test'){
-	obs_prediction=data.frame('allocation'=pred$allocation,'post.prob'=pred$post.prob,'true_status'=convert2bar(obs_prior[,migration]),'migration'=obs_prior[,migration])
+	obs_prediction=data.frame('allocation'=pred$allocation,'post.prob'=pred$post.prob,
+							  'true_status'=convert2bar(obs_prior[,migration]),'migration'=obs_prior[,migration],
+							  'tree_vote'=pred$vote[,'barrier']/ntree) ### new feature !!!! to test
 	obs_prediction$post.prob[which(obs_prediction$allocation=='non-barrier')]=1-obs_prediction$post.prob[which(obs_prediction$allocation=='non-barrier')]
-	
+	### roc curve computation	
 	threshold_seq=seq(0,1,by=roc_smooth)
 	roc_stat=as.data.frame(do.call(rbind,lapply(threshold_seq,function(x,...) get_roc(prior=obs_prediction$migration,est=obs_prediction$post.prob,trsh=x))))
-	if(migration=='null_mig'){obs_AUC=0.5}else{
-	obs_AUC=auc(roc_stat$FPR,roc_stat$TPR,absolutearea=T)}
+	if(migration=='null_mig'){obs_AUC=NA}else{obs_AUC=auc(roc_stat$FPR,roc_stat$TPR,absolutearea=T)}
 	if(is.na(obs_AUC)){obs_AUC=mean(roc_stat$TPR)/(mean(roc_stat$TPR)+mean(roc_stat$FPR))}
-
-	p_barrier_obs_obs=length(which(obs_prediction$true_status=='barrier'))/nrow(obs_prediction)
+	### computing bayes factor ####
+	obs_prediction$bayes_factor=correct_mean_pbarrier_ratio * (obs_prediction$post.prob/(1-obs_prediction$post.prob))
+	#p_barrier_obs_obs=length(which(obs_prediction$true_status=='barrier'))/nrow(obs_prediction)
 	#### bayes part ###
-	obs_prediction$bayes_factor=mean_prior_ratio * (obs_prediction$post.prob/(1-obs_prediction$post.prob))
-	obs_bayes_stat=get_roc(prior=obs_prior[,migration],est=obs_prediction$bayes_factor,trsh=1.000001)
-	names(obs_bayes_stat)=paste0('obs_bayes_',names(obs_bayes_stat))
-	threshold_seq=seq(0,max(obs_prior[,migration]),by=roc_smooth)
 } else {
 	obs_prediction=data.frame('allocation'=pred$allocation,'post.prob'=pred$post.prob)
 	obs_prediction$post.prob[which(obs_prediction$allocation=='non-barrier')]=1-obs_prediction$post.prob[which(obs_prediction$allocation=='non-barrier')]
-	obs_prediction$bayes_factor=mean_prior_ratio * (obs_prediction$post.prob/(1-obs_prediction$post.prob))
+
 }
+
+### computing bayes factor ####
+obs_prediction$bayes_factor=correct_mean_pbarrier_ratio * (obs_prediction$post.prob/(1-obs_prediction$post.prob))
+obs_prediction$BF_approxQ=…
+# To continue 09/11/23 
 
 p_barrier_obs_est=length(which(obs_prediction$allocation=='barrier'))/nrow(obs_prediction)
 
