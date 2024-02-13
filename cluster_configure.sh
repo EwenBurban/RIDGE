d160d25b63bc844348e8d51d09810d916d047247
EwenBurban <burban_ewen@mailfence.com>
2024-02-13 12:26:13 +0100

    update sif list

--- cluster_configure.sh
+++ cluster_configure.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-singularity build --remote container/bcftools.sif container/bcftools.def 
 singularity build --remote container/python.sif container/python.def 
-singularity build --remote container/shapeit.sif container/shapeit.def 
 singularity build --remote container/R.sif container/R.def 
 singularity build --remote container/scrm_py.sif container/scrm_py.def
-#singularity build --remote container/raisd.sif container/raisd.def
-#singularity build --remote container/pcadapt.sif container/pcadapt.def
 singularity build --remote container/R_visual.sif container/R_visual.def
-#singularity build --remote container/plink.sif docker://biocontainers/plink1.9:v1.90b6.6-181012-1-deb_cv1
--- configure.sh
+++ configure.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
-sudo singularity build container/bcftools.sif container/bcftools.def 
 sudo singularity build container/python.sif container/python.def 
-sudo singularity build container/shapeit.sif container/shapeit.def 
 sudo singularity build container/R.sif container/R.def 
 sudo singularity build container/scrm_py.sif container/scrm_py.def
-sudo singularity build container/raisd.sif container/raisd.def
-sudo singularity build container/pcadapt.sif container/pcadapt.def
 sudo singularity build container/R_visual.sif container/R_visual.def
-sudo singularity build container/plink.sif docker://biocontainers/plink1.9:v1.90b6.6-181012-1-deb_cv1
